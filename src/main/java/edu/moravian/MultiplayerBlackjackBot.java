9a516e85f34135d3248eb7ac44a7af63d8153e0d
Aiden <clarea@moravian.edu>
2025-04-08 08:16:01 -0400

    Updated the order in which the database and the dump file is created

--- src/main/java/edu/moravian/MultiplayerBlackjackBot.java
+++ src/main/java/edu/moravian/MultiplayerBlackjackBot.java
@@ -31,7 +31,6 @@ public class MultiplayerBlackjackBot extends ListenerAdapter {
     public MultiplayerBlackjackBot() {
         DatabaseManager databaseManager = new DatabaseManager();
         databaseManager.createDatabaseIfNotExists();
-        databaseManager.createdumpfile();
         databaseManager.importDumpFile();
         databaseManager.createdumpfile();
         messageHandler = new DiscordMessageHandler(databaseManager, games, deck);
