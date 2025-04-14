c1dbe6b1444aa64064f455bd37f31174ff96e531
Kristian <Wrightk@moravian.edu>
2025-04-14 02:22:23 -0400

    Update gojodata.sh

--- gojodata.sh
+++ gojodata.sh
@@ -3,5 +3,5 @@ yum install git -y
 yum install -y maven-amazon-corretto21
 dnf install mariadb105 -y
 dnf install mariadb105-server -y
-systemctl start mariadb
 systemctl enable mariadb
+systemctl start mariadb
