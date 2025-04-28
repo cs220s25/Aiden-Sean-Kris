1e75b51df3c5ac8bf4340307abc8deccfe0d1e7f
Kristian <Wrightk@moravian.edu>
2025-04-28 17:56:18 -0400

    Update localsetup.sh

--- localsetup.sh
+++ localsetup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Clone the repo
-git clone https://github.com/cs220s25/Aiden-Sean-Kris.git
-cd Aiden-Sean-Kris
-
-# Create the .env file (replace YOUR_DISCORD_TOKEN_HERE)
-echo "DISCORD_TOKEN=YOUR_DISCORD_TOKEN_HERE" > .env
-
 # Build the Project
 mvn clean package
 
